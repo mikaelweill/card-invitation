953ce87d3bddd409ba94aedf4d39202ca439baca
Mikael Weill <weillmikael@gmail.com>
2023-03-17 10:11:45 -0700

    pushing all files to github

---
+++ components/header.js
@@ -0,0 +1,9 @@
+export default function Header() {
+    return (
+      <div className="text-center mt-5 mb-16">
+          <h2 className="text-5xl font-bold">
+            CREATE A CARD
+          </h2>
+      </div>
+    )
+  }
